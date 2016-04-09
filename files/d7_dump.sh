820c12c13c7afa15a4160f91f156511890ff4a5b
Logan Cox <lmc@ou.edu>
2016-04-08 21:24:39 -0500

    Fix spellcheck gripes.

--- files/d7_dump.sh
+++ files/d7_dump.sh
@@ -2,8 +2,7 @@
 ## Sync Drupal files & DB from source host
 PATH=/opt/d7/bin:/usr/local/bin:/usr/bin:/bin:/sbin:$PATH
 
-# Writable dir on both local and souce hosts
-TEMPDIR=/var/local/backups/drupal/temp
+source /opt/d7/etc/d7-conf.sh
 
 ## Require arguments
 if [ ! -z "$1" ]
@@ -17,14 +16,14 @@ fi
 
 ## Init site if it doesn't exist
 if [[ ! -e $SITEPATH ]]; then
-    sudo d7_init.sh $SITEPATH || exit 1;
+    sudo d7_init.sh "$SITEPATH" || exit 1;
 fi
 
 ## Grab the basename of the site to use in a few places.
-SITE=`basename $SITEPATH`
+SITE=$(basename "$SITEPATH")
 
 ## Make the database dump directory
-sudo -u apache mkdir -p $SITEPATH/db
+sudo -u apache mkdir -p "$SITEPATH/db"
 
 ## Perform sql-dump
-sudo -u apache drush -r $SITEPATH/drupal sql-dump --result-file=$SITEPATH/db/drupal_$SITE.sql
+sudo -u apache drush -r "$SITEPATH/drupal" sql-dump --result-file="$SITEPATH/db/drupal_$SITE.sql"
