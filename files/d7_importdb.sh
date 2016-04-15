7ad143f27379095c089fd9d2e5745e89ddb13a66
Logan Cox <lmc@ou.edu>
2016-04-15 09:56:11 -0500

    Add optional import of db file from specified path.

--- files/d7_importdb.sh
+++ files/d7_importdb.sh
@@ -5,28 +5,35 @@ PATH=/opt/d7/bin:/usr/local/bin:/usr/bin:/bin:/sbin:$PATH
 source /opt/d7/etc/d7_conf.sh
 
 ## Require arguments
-if [ ! -z "$1" ] 
+if [  -z "$1" ]
 then
-    SITEPATH=$1
-    echo "Importing SITEPATH content from $DUMP"
-else
-  echo "Requires site path (eg. /srv/sample)."
-  exit 1;
+   echo "Requires site path (eg. /srv/sample)."
+   exit 1;
+fi
+SITEPATH=$1
+
+if [[ ! -e $SITEPATH ]]; then
+    echo "No site exists at ${SITEPATH}."
+    exit 1;
 fi
 
 ## Grab the basename of the NEW site to use in a few places.
 SITE=$(basename "$SITEPATH")
 
-## Init site if it doesn't exist
-if [[ ! -e $SITEPATH ]]; then
-    echo "No site exists at ${SITEPATH}."
-    exit
-fi
+if [[ ! -z "$2" ]]
+then
+    DBFILE=$2
+else
+    DBFILE="${SITEPATH}/db/drupal_${SITE}_dump.sql"
+fi       
 
 
 ## Load sql-dump to local DB
-sudo -u apache drush sql-cli -r "$SITEPATH/drupal" < "$SITEPATH/db/drupal_${SITE}_dump.sql" || exit 1;
+
+echo "Synching database for $SITE from file at $DBFILE."
+sudo -u apache drush sql-cli -r "$SITEPATH/drupal" < "${DBFILE}" || exit 1;
 echo "Database synced."
+echo
 
 ## Apply security updates and clear caches.
 d7_update.sh "$SITEPATH" || exit 1;
