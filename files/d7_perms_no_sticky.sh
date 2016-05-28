ad79f70178db0ce6ca6806a37e15304b3d0addce
Jason Sherman <jsnshrmn@users.noreply.github.com>
2016-05-28 12:48:45 -0500

    significant redo of files/d7_perms_no_sticky.sh

--- files/d7_perms_no_sticky.sh
+++ files/d7_perms_no_sticky.sh
@@ -6,16 +6,38 @@ PATH=/opt/d7/bin:/usr/local/bin:/usr/bin:/bin:/sbin:$PATH
 if [ ! -z "$1" ]
 then
   INPUTDIR=$1
-  echo "Processing $INPUTDIR"
+  echo "Processing ${INPUTDIR}"
 else
   echo "Requires input dir (eg. /srv/example/drupal) as argument"
   exit 1;
 fi
 
-## Set perms. Try as apache first, then as self.  Only show errors if both attempts have failed.
-sudo -u apache chmod u=rwx,g=rx,o= $INPUTDIR 2>/dev/null || chmod u=rwx,g=rx,o= $INPUTDIR
-find "$INPUTDIR" -type d -exec bash -c "sudo -u apache chmod u=rwx,g=rx,o= '{}' 2>/dev/null || chmod u=rwx,g=rx,o= '{}'" \;
-find "$INPUTDIR" -type f -exec bash -c "sudo -u apache chmod u=rw,g=r,o= '{}' 2>/dev/null || chmod u=rw,g=r,o= '{}'" \;
-sudo semanage fcontext -a -t httpd_sys_content_t  "$INPUTDIR(/.*)?"
-sudo restorecon -R "$INPUTDIR"
-sudo -u apache chgrp -R apache $INPUTDIR 2>/dev/null || chgrp -R apache $INPUTDIR
+DIRPERMS='u=rwx,g=rx,o='
+FILEPERMS='u=rw,g=r,o='
+
+## Set perms. Try as apache first, then as self.
+
+## Loop through the subdirectories
+for SUBDIR in `find "${INPUTDIR}" -mindepth 1 -maxdepth 1 -type d`
+do
+  sudo -u apache chmod ${DIRPERMS} "${SUBDIR}" 2>/dev/null ||\
+  chmod ${DIRPERMS} "${SUBDIR}" 2>/dev/null
+done
+
+## Loop through the files
+for FILE in `find "${INPUTDIR}" -mindepth 1 -maxdepth 1 -type f`
+do
+  sudo -u apache chmod ${FILEPERMS} "${FILE}" 2>/dev/null ||\
+  chmod ${FILEPERMS} "${FILE}" 2>/dev/null
+done
+
+## This stuff may not do anything on NFS
+sudo semanage fcontext -a -t httpd_sys_content_t  "${INPUTDIR}(/.*)?"
+sudo restorecon -R "${INPUTDIR}"
+sudo -u apache chgrp -R apache "${INPUTDIR}" 2>/dev/null ||\
+chgrp -R apache "${INPUTDIR}" 2>/dev/null
+
+## Set perms for input directory itself. Try as apache first, then as self.
+## For no reason I can fathom, this must come last.
+sudo -u apache chmod ${DIRPERMS} "${INPUTDIR}" ||\
+chmod ${DIRPERMS} "${INPUTDIR}"
