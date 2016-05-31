2a8bec72cd21cbc65bf5e6c329620e9bcda9f9c8
Jason Sherman <jsnshrmn@users.noreply.github.com>
2016-05-31 10:36:06 -0500

    Don't die on perms failure in files/d7_migrate.sh

--- files/d7_migrate.sh
+++ files/d7_migrate.sh
@@ -30,7 +30,7 @@ done
 
 # Perms
 echo "Setting permissions for config files."
-d7_perms_sticky.sh "$SITEPATH/etc" || exit 1;
+d7_perms_sticky.sh "$SITEPATH/etc"
 
 # Install modules and themes
 d7_make.sh "$SITEPATH" || exit 1
