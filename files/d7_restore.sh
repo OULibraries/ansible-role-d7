9ba5371b805190745544f503eca42b8bdc736e59
Logan Cox <lmc@ou.edu>
2016-05-31 20:51:36 -0500

    Better docs and arg handling.

--- files/d7_restore.sh
+++ files/d7_restore.sh
@@ -5,7 +5,8 @@ PATH=/opt/d7/bin:/usr/local/bin:/usr/bin:/bin:/sbin:$PATH
 ## Require arguments
 if [  -z "$1" ]
 then
-    echo "Usage: d7_snapshot.sh \$SITEPATH"
+    echo "Usage: d7_snapshot.sh \$SITEPATH \$DOW"
+    echo "$DOW should be something like Mon, Tue, Web, etc. "
     exit 1;
 fi
 SITEPATH=$1
@@ -13,5 +14,28 @@ DOW=$2
 SITE=$(basename "$SITEPATH")
 SNAPSHOTFILE="${SITEPATH}/snapshots/${SITE}.${DOW}.tar"
 
-# extract files for site from snapshot
-sudo -u apache tar -xvf "${SNAPSHOTFILE}" -C "${SITEPATH}"
+if [ -z "${DOW}" ]; then
+    echo "No snapshot specified."
+    echo "The following snapshots exist:"
+    ls "${SITEPATH}/snapshots/"
+    exit 0
+fi
+
+
+if [ ! -f $SNAPSHOTFILE ]; then
+    echo "No snapshot at ${SNAPSHOTFILE}"
+    exit 0
+fi
+
+# It's possible we're restoring a deleted site 
+mkdir -p "${SITEPATH}"
+
+# Tarballs include the $SITE folder, so we need to strip that off
+# whene extracting
+sudo -u apache tar -xf "${SNAPSHOTFILE}" -C "${SITEPATH}" --strip-components=1
+
+
+echo "Files from snapshot restored." 
+echo "Now run d7_importdb.sh ${SITEPATH} to restore the db for the site."
+
+
