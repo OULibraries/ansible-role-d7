e3bd7021dc795259cd98b28ffaaf277f3f9dc164
vagrant <vagrant@localhost.localdomain>
2016-06-17 00:02:42 -0500

    Make dev mode use sticky perms everywhere

--- files/d7_perms_no_sticky.sh
+++ files/d7_perms_no_sticky.sh
@@ -2,24 +2,36 @@
 ## Set permissions on the specified dir
 PATH=/opt/d7/bin:/usr/local/bin:/usr/bin:/bin:/sbin:$PATH
 
+source /opt/d7/etc/d7_conf.sh
+
 ## Require arguments
-if [ ! -z "$1" ]
-then
-  INPUTDIR=$1
-else
+if [  -z "$1" ]; then
   echo "Requires input dir (eg. /srv/example/drupal) as argument"
   exit 1;
 fi
 
-DIRPERMS='u=rwx,g=rx,o='
-FILEPERMS='u=rw,g=r,o='
+INPUTDIR=$1
+
+if [[ ${ENV_NAME} == *dev ]] || [[ $0 != *no_sticky.sh ]]; then 
+  # content files, settings and dev mode drupal files get group write
+  # and sticky bit
+  DIRPERMS='u=rwxs,g=rwxs,o='
+  FILEPERMS='u=rw,g=rw,o='
+  POLICY="sticky"
+else
+  # More restrictive perms for drupal files in prod mode
+  DIRPERMS='u=rwx,g=rx,o='
+  FILEPERMS='u=rw,g=r,o='
+  POLICY="no sticky"
+fi
+
 
 if [ ! -d "$INPUTDIR" ]; then
   echo "cannot access ${INPUTDIR}: No such directory"
   exit 1
 fi
 
-echo "Setting permissions on ${INPUTDIR}"
+echo "Setting ${POLICY} permissions on ${INPUTDIR}"
 
 ## Set SELinux context.  Useless over NFS/SMB.
 sudo semanage fcontext -a -t httpd_sys_content_t  "${INPUTDIR}(/.*)?"
