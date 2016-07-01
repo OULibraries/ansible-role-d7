2b2f3ffcd301de66cebea3b33f4a7c7eb659cd0e
Logan Cox <lmc@ou.edu>
2016-06-30 23:30:58 -0500

    Add approriate whitespace.

--- files/d7_cc.sh
+++ files/d7_cc.sh
@@ -30,7 +30,7 @@ APC_PASS=$(cat /usr/share/pear/apc.php  | grep ^defaults.*PASSWORD | grep -o \'.
 APC_PASS=${APC_PASS:1:-1}
 
 echo "Clearing APC cache"
-curl --silent --basic --user "${APC_USER}:${APC_PASS}" "http://localhost/apc.php?SCOPE=A&SORT1=H&SORT2=D&COUNT=20&CC=1&OB=1" >/dev/null|| exit 1;
+curl --silent --basic --user "${APC_USER}:${APC_PASS}" "http://localhost/apc.php?SCOPE=A&SORT1=H&SORT2=D&COUNT=20&CC=1&OB=1" >/dev/null || exit 1;
 
 echo "Clearing Drupal caches for ${SITEPATH}."
 sudo -u apache drush -y cc all -r "$SITEPATH/drupal" || exit 1;
