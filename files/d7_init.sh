b3f5da54f2203233cb97b0c825b48ac029037d21
Logan Cox <lmc@ou.edu>
2016-12-21 20:46:33 -0600

    Comment command.

--- files/d7_init.sh
+++ files/d7_init.sh
@@ -65,8 +65,8 @@ sudo -u apache mv "$SITEPATH/drupal/sites/default" "$SITEPATH"/
 echo "Linking default site into build."
 sudo -u apache ln -s "$SITEPATH/default" "$SITEPATH/drupal/sites/default" || exit 1;
 
-
-DBSLUG=$(echo  "${SITE}" | tr -c 'A-Za-z_0-9' '_')
+## Sanitize the DB slug by excluding everything that MySQL doesn't like
+DBSLUG=$(echo -n  "${SITE}" | tr -C '_A-Za-z0-9' '_')
 
 echo "Generating settings.php with database ${DBSLUG}."
 read -r -d '' SETTINGSPHP <<- EOF
