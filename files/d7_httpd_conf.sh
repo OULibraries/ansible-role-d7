4494feaa03ae949a35ada045c5f9ea153a0d33e7
Jason Sherman <jsnshrmn@users.noreply.github.com>
2017-02-09 11:47:26 -0600

    Capture work to make d7_httpd_conf.sh more flexible.

--- files/d7_httpd_conf.sh
+++ files/d7_httpd_conf.sh
@@ -1,21 +1,35 @@
 #!/usr/bin/env bash
+
 ## Create Apache config for Drupal site
 
 source /opt/d7/etc/d7_conf.sh
 
 if [  -z "$1" ]; then
+
   cat <<USAGE
+
 d7_httpd_conf.sh generates the Apache config for a site.
 
-Usage: d7_httpd_conf.sh \$SITEPATH
-            
+Usage: d7_httpd_conf.sh \$SITEPATH [\$SITE_TYPE]
+
 \$SITEPATH  Drupal site.
+\$SITE_TYPE  optional argument, standalone (default), master, or sub. 
+
 USAGE
 
   exit 1;
+
 fi
 
 SITEPATH=$1
+
+if [ ! -z "$2" ]; then
+  if [ "$2" == "standalone" ] || [ "$2" == "master" ] || [ "$2" == "sub" ]; then
+    SITE_TYPE=$2 
+  fi
+else
+    SITE_TYPE=standalone
+fi
 
 ## Site should already be there
 if [[ ! -e $SITEPATH ]]; then
@@ -28,12 +42,46 @@ SITE=$(basename "$SITEPATH")
 
 ## Make the apache config
 echo "Generating Apache config for ${SITEPATH}."
-
 sudo -u apache mkdir -p "$SITEPATH/etc"
 
-sudo -u apache sh -c "sed "s/__SITE_DIR__/$SITE/g" /opt/d7/etc/d7_init_httpd_template > $SITEPATH/etc/srv_$SITE.conf" || exit 1;
-sudo -u apache sh -c "sed -i "s/__SITE_NAME__/$SITE/g" $SITEPATH/etc/srv_$SITE.conf" || exit 1;
+## Standalone config gets our original template
+if [ "$SITE_TYPE" == "standalone" ]; then
+  sudo -u apache sh -c "sed "s/__SITE_DIR__/$SITE/g" /opt/d7/etc/d7_init_httpd_template > $SITEPATH/etc/srv_$SITE.conf" || exit 1;
+  sudo -u apache sh -c "sed -i "s/__SITE_NAME__/$SITE/g" $SITEPATH/etc/srv_$SITE.conf" || exit 1;
 
+# Master gets a special interactive configuration
+
+elif [ "$SITE_TYPE" == "master" ]; then
+  echo "Generating ${SITEPATH}/etc/srv_${SITE}.conf with additional subsite information."
+  read -r -d '' SRV_SITE_CONF <<- EOF
+<VirtualHost *:443>
+
+  Include /etc/httpd/conf.d/00ssl.include
+  SSLCertificateFile \${HTTPD_CERT_PATH}/star.\${HTTPD_DN_SUFFIX}/cert.pem
+  SSLCertificateKeyFile \${HTTPD_KEY_PATH}/star.\${HTTPD_DN_SUFFIX}/privkey.pem
+  SSLCertificateChainFile \${HTTPD_CERT_PATH}/star.\${HTTPD_DN_SUFFIX}/chain.pem
+
+  ServerName ${SITE}.\${HTTPD_DN_SUFFIX}
+  DocumentRoot /srv/${SITE}/drupal
+
+  <Directory "/srv/${SITE}/drupal">
+    Options Indexes FollowSymLinks
+    AllowOverride All
+    Order allow,deny
+    Allow from all
+    Require all granted
+  </Directory>
+
+</VirtualHost>
+EOF
+
+  sudo -u apache echo "$SRV_SITE_CONF"| sudo -u apache tee -a "$SITEPATH/etc/srv_$SITE.conf" >/dev/null
+
+# Subsite gets an empty configuration
+elif [ "$SITE_TYPE" == "sub" ]; then
+  sudo -u apache sh -c "echo \# > "$SITEPATH/etc/srv_$SITE.conf" || exit 1;
+fi
+  
 ## Allow apache to read its config
 sudo semanage fcontext -a -t httpd_sys_rw_content_t  "$SITEPATH/etc(/.*)?" || exit 1;
 sudo restorecon -R "$SITEPATH/etc" || exit 1;
